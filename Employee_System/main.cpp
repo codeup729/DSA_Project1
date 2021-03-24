0438a6f8d49157092ef061f92512c4b85f2e522f
Anitej Srivastava <anitejsri22@gmail.com>
2021-03-24 09:16:52 +0530

    Initial Commit

---
+++ Employee_System/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Employee_System
+//
+//  Created by Anitej Srivastava on 24/03/21.
+//  Copyright © 2021 Anitej Srivastava. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
